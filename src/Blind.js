51944356fe7d6ba0f2363bad748535be896a7c22
vinay29102002 <144591351+Vinaydesh29@users.noreply.github.com>
2025-06-04 21:50:06 +0530

    update for today

--- src/Blind.js
+++ src/Blind.js
@@ -1,7 +1,17 @@
 function Blind(){
-   const val = ()=>{
-    console.log("hello" + this.name);
-   }
-   const obj = {name:"sai"}
+    let user1 = {
+        name:"arjun",
+        age:"20",
+        func: function(){
+            console.log(this.name);
+            
+        }
+    }
+    let user2 = {
+        name:"sai",
+        age:"21",
+       
+    }
+  user1.func.call(user2)
 }
 export default Blind
